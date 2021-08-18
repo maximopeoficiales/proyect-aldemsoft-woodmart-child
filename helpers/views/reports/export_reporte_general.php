02a80ec6fe6db6db143633b477a12b76bdf1e8c3
Maximo Junior Apaza Chirhuana <maximopeoficiales@gmail.com>
2021-08-18 05:06:31 -0500

    falta cerrar huecos

--- helpers/views/reports/export_reporte_general.php
+++ helpers/views/reports/export_reporte_general.php
@@ -19,7 +19,7 @@
 
     .aldem-bg-gray-white {
         background-color: #D6DCE4;
-        color: #121212;
+        color: #344176;
 
     }
 
@@ -45,14 +45,36 @@
         <table class="table table-bordered table-striped text-center table-report-aldem">
             <thead>
                 <tr>
-                    <th colspan="18" class="aldem-bg-gray">TARIFA PICK UP (RECOJO DE MUESTRAS)</th>
-                    <th colspan="18" class="aldem-bg-gray">TARIFA HIELO SECO</th>
+                    <th colspan="36" class="aldem-bg-gray">TARIFA PICK UP (RECOJO DE MUESTRAS)</th>
+                    <th colspan="36" class="aldem-bg-gray">TARIFA HIELO SECO</th>
                 </tr>
                 <tr>
                     <th colspan="17" class="aldem-bg-blue">CANTIDAD DE PICK UP (RECOJOS)
                     </th>
                     <th rowspan="4" colspan="1" class="aldem-bg-yellow">TOTAL GUIAS MARKEN</th>
-                    <th colspan="18" class="aldem-bg-blue">COBRO A MARKEN PICK UP DOLARES USD</th>
+                    <th colspan="17" class="aldem-bg-blue">COBRO A MARKEN PICK UP DOLARES USD</th>
+                    <th rowspan="4" colspan="1" class="aldem-bg-yellow">TOTAL PICK UP DOLARES USD</th>
+
+                    <!-- 36 -->
+                    <th rowspan="4" colspan="3" class="aldem-bg-gray">CANTIDAD DE HIELO SECO KG PARA MUESTRAS EXPORTACION X MAWB</th>
+                    <!-- 33 -->
+                    <th rowspan="4" colspan="10" class="aldem-bg-gray">Observaciones</th>
+                    <!-- 23 -->
+                    <th rowspan="4" colspan="3" class="aldem-bg-gray">COSTO (INVERSIÓN)DE HIELO SECO SECO EN LIMA SOLES(SIN IGV)</th>
+                    <!-- 20 -->
+                    <th rowspan="4" colspan="3" class="aldem-bg-gray">COSTO (INVERSIÓN)DE HIELO SECO EN LIMA DOLARES TIPO DE CAMBIO: @TC</th>
+                    <!-- 17 -->
+                    <th rowspan="2" colspan="6" class="aldem-bg-gray">TARIFA DE HIELO SECO DOLARES</th>
+                    <!-- 11 -->
+                    <th rowspan="2" colspan="2" class="aldem-bg-gray-white">UTILIDAD HIELO SECO DOLARES</th>
+                    <!-- 9 -->
+                    <th rowspan="2" colspan="2" class="aldem-bg-gray-white">HANDLING</th>
+                    <!-- 7 -->
+                    <th rowspan="4" colspan="1" class="aldem-bg-gray-white">UTILIDAD HANDLING DOLARES</th>
+                    <!-- 6 -->
+                    <th rowspan="4" colspan="2" class="aldem-bg-blue">TARIFA DE TRAMITE OPERATIVO DOLARES</th>
+                    <th rowspan="4" colspan="2" class="aldem-bg-blue">TARIFA DE CAJA DE EMBALAJE MUESTRAS DE AMBIENTE DOLARES</th>
+                    <th rowspan="4" colspan="2" class="aldem-bg-gray-white">COSTO DE ALDEM POR EMBALAR (Costo del tiempo que se usa para embalar las cajas) DOLARES</th>
                 </tr>
 
                 <tr>
@@ -61,19 +83,16 @@
                     <th colspan="8" class="aldem-bg-gray">GUIAS DE PROVINCIA</th>
                     <th colspan="8" class="aldem-bg-gray">GUIAS DE LIMA</th>
                     <th colspan="8" class="aldem-bg-gray">GUIAS DE PROVINCIA</th>
+                    <!-- hay uno  -->
                 </tr>
                 <tr>
                     <th colspan="8" class="aldem-bg-gray">FROZEN</th>
                     <th colspan="8" class="aldem-bg-gray">FROZEN</th>
                     <th colspan="8" class="aldem-bg-gray">FROZEN</th>
                     <th colspan="8" class="aldem-bg-gray">FROZEN</th>
+
                 </tr>
                 <tr>
-                    <th colspan="2" class="aldem-bg-gray">Ambiente</th>
-                    <th colspan="2" class="aldem-bg-gray">BIO I</th>
-                    <th colspan="2" class="aldem-bg-gray">BIO II</th>
-                    <th colspan="2" class="aldem-bg-gray">BIO II</th>
-                    
                     <th colspan="2" class="aldem-bg-gray">Ambiente</th>
                     <th colspan="2" class="aldem-bg-gray">BIO I</th>
                     <th colspan="2" class="aldem-bg-gray">BIO II</th>
@@ -83,17 +102,22 @@
                     <th colspan="2" class="aldem-bg-gray">BIO I</th>
                     <th colspan="2" class="aldem-bg-gray">BIO II</th>
                     <th colspan="2" class="aldem-bg-gray">BIO II</th>
-                    
+
                     <th colspan="2" class="aldem-bg-gray">Ambiente</th>
                     <th colspan="2" class="aldem-bg-gray">BIO I</th>
                     <th colspan="2" class="aldem-bg-gray">BIO II</th>
                     <th colspan="2" class="aldem-bg-gray">BIO II</th>
-                    
-                   
+
+                    <th colspan="2" class="aldem-bg-gray">Ambiente</th>
+                    <th colspan="2" class="aldem-bg-gray">BIO I</th>
+                    <th colspan="2" class="aldem-bg-gray">BIO II</th>
+                    <th colspan="2" class="aldem-bg-gray">BIO II</th>
+
+
                 </tr>
             </thead>
             <tbody>
-                
+
             </tbody>
         </table>
     </div>
