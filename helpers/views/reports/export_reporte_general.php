bf4769724955fc233a491586fbb51867b5e8155a
Maximo Junior Apaza Chirhuana <maximopeoficiales@gmail.com>
2021-08-18 06:20:21 -0500

    ADD: Se termino el diseño

--- helpers/views/reports/export_reporte_general.php
+++ helpers/views/reports/export_reporte_general.php
@@ -7,7 +7,7 @@
 
     .aldem-bg-black-white {
         background-color: #808080;
-        color: #121212;
+        color: #fff;
 
     }
 
@@ -38,6 +38,14 @@
         background-color: #2F75B5;
         color: #fff;
     }
+
+    .aldem-text-white {
+        color: #fff;
+    }
+
+    .aldem-text-black {
+        color: #121212;
+    }
 </style>
 <div class="row" style="overflow-x: scroll;">
     <!-- 36 -->
@@ -45,8 +53,8 @@
         <table class="table text-center table-report-aldem">
             <thead>
                 <tr>
-                    <th colspan="36" class="aldem-bg-gray">TARIFA PICK UP (RECOJO DE MUESTRAS)</th>
-                    <th colspan="36" class="aldem-bg-gray">TARIFA HIELO SECO</th>
+                    <th colspan="36" class="aldem-bg-gray aldem-text-white">TARIFA PICK UP (RECOJO DE MUESTRAS)</th>
+                    <th colspan="39" class="aldem-bg-gray aldem-text-white">TARIFA HIELO SECO</th>
                 </tr>
                 <tr>
                     <th colspan="17" class="aldem-bg-blue">CANTIDAD DE PICK UP (RECOJOS)
@@ -56,13 +64,13 @@
                     <th rowspan="4" colspan="1" class="aldem-bg-yellow">TOTAL PICK UP DOLARES USD</th>
 
                     <!-- 36 -->
-                    <th rowspan="4" colspan="3" class="aldem-bg-gray">CANTIDAD DE HIELO SECO KG PARA MUESTRAS EXPORTACION X MAWB</th>
+                    <th rowspan="4" colspan="3" class="aldem-bg-black-white">CANTIDAD DE HIELO SECO KG PARA MUESTRAS EXPORTACION X MAWB</th>
                     <!-- 33 -->
-                    <th rowspan="4" colspan="10" class="aldem-bg-gray">Observaciones</th>
+                    <th rowspan="4" colspan="10" class="aldem-bg-black-white">Observaciones</th>
                     <!-- 23 -->
-                    <th rowspan="4" colspan="3" class="aldem-bg-gray">COSTO (INVERSIÓN)DE HIELO SECO SECO EN LIMA SOLES(SIN IGV)</th>
+                    <th rowspan="4" colspan="3" class="aldem-bg-black-white">COSTO (INVERSIÓN)DE HIELO SECO SECO EN LIMA SOLES(SIN IGV)</th>
                     <!-- 20 -->
-                    <th rowspan="4" colspan="3" class="aldem-bg-gray">COSTO (INVERSIÓN)DE HIELO SECO EN LIMA DOLARES TIPO DE CAMBIO: @TC</th>
+                    <th rowspan="4" colspan="3" class="aldem-bg-black-white">COSTO (INVERSIÓN)DE HIELO SECO EN LIMA DOLARES TIPO DE CAMBIO: @TC</th>
                     <!-- 17 -->
                     <th rowspan="2" colspan="6" class="aldem-bg-gray">TARIFA DE HIELO SECO DOLARES</th>
                     <!-- 11 -->
@@ -74,24 +82,24 @@
                     <!-- 6 -->
                     <th rowspan="4" colspan="2" class="aldem-bg-blue">TARIFA DE TRAMITE OPERATIVO DOLARES</th>
                     <th rowspan="4" colspan="2" class="aldem-bg-blue">TARIFA DE CAJA DE EMBALAJE MUESTRAS DE AMBIENTE DOLARES</th>
-                    <th rowspan="4" colspan="2" class="aldem-bg-gray-white">COSTO DE ALDEM POR EMBALAR (Costo del tiempo que se usa para embalar las cajas) DOLARES</th>
+                    <th rowspan="4" colspan="5" class="aldem-bg-gray-white">COSTO DE ALDEM POR EMBALAR (Costo del tiempo que se usa para embalar las cajas) DOLARES</th>
                 </tr>
 
                 <tr>
                     <th colspan="1" rowspan="3" class="aldem-bg-gray">Fecha</th>
                     <th colspan="8" class="aldem-bg-gray">GUIAS DE LIMA</th>
-                    <th colspan="8" class="aldem-bg-gray">GUIAS DE PROVINCIA</th>
+                    <th colspan="8" class="aldem-bg-black-white">GUIAS DE PROVINCIA</th>
                     <th colspan="8" class="aldem-bg-gray">GUIAS DE LIMA</th>
-                    <th colspan="9" class="aldem-bg-gray">GUIAS DE PROVINCIA</th>
+                    <th colspan="9" class="aldem-bg-black-white">GUIAS DE PROVINCIA</th>
                 </tr>
                 <tr>
                     <th colspan="8" class="aldem-bg-gray">FROZEN</th>
+                    <th colspan="8" class="aldem-bg-black-white">FROZEN</th>
                     <th colspan="8" class="aldem-bg-gray">FROZEN</th>
-                    <th colspan="8" class="aldem-bg-gray">FROZEN</th>
-                    <th colspan="9" class="aldem-bg-gray">FROZEN</th>
+                    <th colspan="9" class="aldem-bg-black-white">FROZEN</th>
                     <th colspan="3" class="aldem-bg-gray-white">LIMA</th>
                     <th colspan="3" class="aldem-bg-black-white">Provincia</th>
-                    <th colspan="2" class="" rowspan="2">LIMA</th>
+                    <th colspan="2" class="aldem-text-black" rowspan="2">LIMA</th>
                     <th rowspan="2" class="aldem-bg-yellow">TARIFA DOLARES</th>
                     <th rowspan="2" class="aldem-bg-black-white">GASTO DOLARES</th>
 
@@ -102,20 +110,20 @@
                     <th colspan="2" class="aldem-bg-gray">BIO II</th>
                     <th colspan="2" class="aldem-bg-gray">BIO II</th>
 
-                    <th colspan="2" class="aldem-bg-gray">Ambiente</th>
-                    <th colspan="2" class="aldem-bg-gray">BIO I</th>
-                    <th colspan="2" class="aldem-bg-gray">BIO II</th>
-                    <th colspan="2" class="aldem-bg-gray">BIO II</th>
+                    <th colspan="2" class="aldem-bg-black-white">Ambiente</th>
+                    <th colspan="2" class="aldem-bg-black-white">BIO I</th>
+                    <th colspan="2" class="aldem-bg-black-white">BIO II</th>
+                    <th colspan="2" class="aldem-bg-black-white">BIO II</th>
 
                     <th colspan="2" class="aldem-bg-gray">Ambiente</th>
                     <th colspan="2" class="aldem-bg-gray">BIO I</th>
                     <th colspan="2" class="aldem-bg-gray">BIO II</th>
                     <th colspan="2" class="aldem-bg-gray">BIO II</th>
 
-                    <th colspan="2" class="aldem-bg-gray">Ambiente</th>
-                    <th colspan="2" class="aldem-bg-gray">BIO I</th>
-                    <th colspan="2" class="aldem-bg-gray">BIO II</th>
-                    <th colspan="3" class="aldem-bg-gray">BIO II</th>
+                    <th colspan="2" class="aldem-bg-black-white">Ambiente</th>
+                    <th colspan="2" class="aldem-bg-black-white">BIO I</th>
+                    <th colspan="2" class="aldem-bg-black-white">BIO II</th>
+                    <th colspan="3" class="aldem-bg-black-white">BIO II</th>
 
                     <!-- 6 espacios para LIMA - PROVINCIA -->
                     <th class="aldem-bg-gray">BIO I</th>
@@ -126,12 +134,46 @@
                     <th class="aldem-bg-black-white">BIO II</th>
                 </tr>
             </thead>
-            <tbody>
+            <tbody style="color:black; text-align: center;">
                 <tr>
-                    <td scope="row"></td>
-                    <td></td>
-                    <td></td>
+                    <td>dato 1</td>
+                    <td colspan="2">dato 2</td>
+                    <td colspan="2">dato 4</td>
+                    <td colspan="2">dato 6</td>
+                    <td colspan="2">dato 8</td>
+                    <td colspan="2">dato 10</td>
+                    <td colspan="2">dato 12</td>
+                    <td colspan="2">dato 15</td>
+                    <td colspan="2">dato 16</td>
+                    <td class="aldem-bg-blue-white">dato 17</td>
+                    <td colspan="2">dato 18</td>
+                    <td colspan="2">dato 20</td>
+                    <td colspan="2">dato 22</td>
+                    <td colspan="2">dato 24</td>
+                    <td colspan="2">dato 26</td>
+                    <td colspan="2">dato 28</td>
+                    <td colspan="2">dato 30</td>
+                    <td colspan="3">dato 31</td>
+                    <td>dato 34</td>
+                    <td colspan="3">dato 35</td>
+                    <td colspan="10">dato 38</td>
+                    <td colspan="3">dato 48</td>
+                    <td colspan="3">dato 51</td>
+                    <td>dato 54</td>
+                    <td>dato 55</td>
+                    <td>dato 56</td>
+                    <td>dato 57</td>
+                    <td>dato 58</td>
+                    <td>dato 59</td>
+                    <td colspan="2">dato 60</td>
+                    <td>dato 62</td>
+                    <td>dato 63</td>
+                    <td>dato 64</td>
+                    <td colspan="2">dato 65</td>
+                    <td colspan="2">dato 67</td>
+                    <td colspan="5">dato 69jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj</td>
                 </tr>
+
             </tbody>
         </table>
     </div>
