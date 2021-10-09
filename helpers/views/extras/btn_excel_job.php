3385db24e58a4da99ee7d7c502ac0a1f366ebfd1
Maximo Junior Apaza Chirhuana <maximopeoficiales@gmail.com>
2021-10-08 19:10:04 -0500

    ADD: se termino logica para insercion atraves de csv

--- helpers/views/extras/btn_excel_job.php
+++ helpers/views/extras/btn_excel_job.php
@@ -1,14 +1,18 @@
 <?php
 aldem_cargarStyles();
-aldem_show_message_custom("Se ha registrado correctamente el Job 😀", "Se ha actualizado correctamente el Job😀", "Ocurrio un error 😢 en la subida del Archivo");
 ?>
 
 
-<form action="<?php echo admin_url('admin-post.php') ?>" method="post" id="formNewExport" enctype="multipart/form-data">
+<!-- <div class="row my-4 justify-content-center"> -->
+<form action="<?php echo admin_url('admin-post.php') ?>" method="post" id="formNewExport" enctype="multipart/form-data" class="my-4">
 
-
+    <?= aldem_set_input_hidden("page", $atts["page"]) ?>
     <?php aldem_set_proccess_form(); ?>
     <?php aldem_set_action_name("upload-excel-job"); ?>
-    <input type="file" name="file_export_jobs" id="">
+    <input type="file" name="file_export_jobs" id="" accept=".csv">
     <button type="submit">Subir CSV</button>
 </form>
+<!-- </div> -->
+
+<?php aldem_show_message_custom("Se ha registrado correctamente el Job 😀", "Se ha actualizado correctamente el Job😀", "Ocurrio un error 😢 en la subida del Archivo");
+?>
